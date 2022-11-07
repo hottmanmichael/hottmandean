ed8c72db099fffedae1b144be801e61aca72b2a4
Michael Hottman <hottmanmichael@gmail.com>
2022-11-07 10:03:53 -0700

    Global fonts

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -26,23 +26,31 @@ export function useMediaQuery(query: string): boolean {
   }
 
   useEffect(() => {
-    const matchMedia = window.matchMedia(query);
+    let matchMedia: null | MediaQueryList;
+
+    if (typeof window !== "undefined") {
+      matchMedia = window.matchMedia(query);
+    }
 
     // Triggered at the first client-side load and if query changes
     handleChange();
 
-    // Listen matchMedia
-    if (matchMedia.addListener) {
-      matchMedia.addListener(handleChange);
-    } else {
-      matchMedia.addEventListener("change", handleChange);
+    if (matchMedia !== null) {
+      // Listen matchMedia
+      if (matchMedia.addListener) {
+        matchMedia.addListener(handleChange);
+      } else {
+        matchMedia.addEventListener("change", handleChange);
+      }
     }
 
     return () => {
-      if (matchMedia.removeListener) {
-        matchMedia.removeListener(handleChange);
-      } else {
-        matchMedia.removeEventListener("change", handleChange);
+      if (matchMedia !== null) {
+        if (matchMedia.removeListener) {
+          matchMedia.removeListener(handleChange);
+        } else {
+          matchMedia.removeEventListener("change", handleChange);
+        }
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
